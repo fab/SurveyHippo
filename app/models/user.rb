39f0f2385830adf4e50e76d15878d850fd6c900a
vincentstorme <storme.vincent@gmail.com>
2013-05-03 15:03:14 -0700

    added ::Base to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::
+class User < ActiveRecord::Base
   has_many :completions
   has_many :surveys, :through => :completions, :class_name => 'Survey', :foreign_key => 'author_id'
   has_many :responses
--- db/migrate/20130503114725_create_responses.rb
+++ db/migrate/20130503114725_create_responses.rb
@@ -1,4 +1,4 @@
-class CreateUserResponses < ActiveRecord::Migration
+class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses, :id => false do |t|
       t.references :user
