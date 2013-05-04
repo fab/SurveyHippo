02dfe4efdf0dff9e4ef21e4d3912ba7a6f22b92c
Fab Mackojc <fab.mackojc@gmail.com>
2013-05-03 17:13:20 -0700

    Added dynamic creation of question and choices in the database for a new survey

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -4,16 +4,22 @@ end
 
 post '/survey/create' do
   p params
-  p params[:question]
-  p params[:q1_choice]
+  # p params[:question]
+  # p params[:q1_choice]
 
   survey = Survey.create(name: params[:name])
 
+  q_num = 1
+
   params[:question].each do |q|
+
     question = Question.create(question: q, survey_id: survey.id)
-    params[:q1_choice].each do |c|
+
+    params["choice_q#{q_num}"].each do |c|
       choice = Choice.create(choice: c, question_id: question.id)
     end
+
+    q_num += 1
   end
 
   # do it for the first elements in choice up until length of the 
