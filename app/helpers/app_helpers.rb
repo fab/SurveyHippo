1e0899f24d3e41b6da4851d5405daf9f08457984
alukima <amylukima@gmail.com>
2013-05-03 18:32:50 -0700

    list completed and created surveys in userprofile; added create survey to user_profile

--- app/helpers/app_helpers.rb
+++ app/helpers/app_helpers.rb
@@ -4,5 +4,11 @@ helpers do
     @current_user ||= User.find(session[:id]) if session[:id]
   end
 
+  def users_created_surveys
+     @created_surveys = Survey.where(author_id: session[:id])
+  end
 
+  def users_completed_surveys
+     @completed_surveys = Survey.where(author_id: session[:id])
+  end
 end
