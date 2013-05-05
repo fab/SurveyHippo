5ba726a4b8d2daa7f1592b8fd3c4d0998c2cedb6
fmackojc <fab.mackojc@gmail.com>
2013-05-05 13:58:00 -0700

    removed comments from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# 100.times do
-#   User.create(:username => Faker::Name.first_name, :password => 'password', :email => Faker::Internet.email)
-# end
+100.times do
+  User.create(:username => Faker::Name.first_name, :password => 'password', :email => Faker::Internet.email)
+end
 
-# 100.times do
-#   Survey.create(:author_id => User.all.sample.id, :name => Faker::Company.bs)
-# end
+100.times do
+  Survey.create(:author_id => User.all.sample.id, :name => Faker::Company.bs)
+end
 
-# 500.times do
-#   Question.create(:survey_id => Survey.all.sample.id, :question => Faker::Company.catch_phrase)
-# end
+500.times do
+  Question.create(:survey_id => Survey.all.sample.id, :question => Faker::Company.catch_phrase)
+end
 
-# 1000.times do
-#   Choice.create(:question_id => Question.all.sample.id, :choice => Faker::Lorem.sentence)
-# end
+1000.times do
+  Choice.create(:question_id => Question.all.sample.id, :choice => Faker::Lorem.sentence)
+end
 
 100.times do |i|
   Response.create(:question_id => 519, :choice_id => 1025, :user_id => i)
