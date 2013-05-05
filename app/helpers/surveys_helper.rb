8b5d052939dfd4925513c909f32bb157a4fd45b4
vincentstorme <storme.vincent@gmail.com>
2013-05-04 20:01:50 -0700

    split up helper file to multiple files

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,17 +1,5 @@
 helpers do 
 
-  def current_user
-    @current_user ||= User.find(session[:id]) if session[:id]
-  end
-
-  def users_created_surveys
-     @created_surveys = Survey.where(author_id: session[:id])
-  end
-
-  def users_completed_surveys
-     @completed_surveys = Survey.where(author_id: session[:id])
-  end
-
   def all_surveys
     @all_surveys = Survey.all
   end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,15 @@
+helpers do 
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def users_created_surveys
+     @created_surveys = Survey.where(author_id: session[:id])
+  end
+
+  def users_completed_surveys
+     @completed_surveys = Survey.where(author_id: session[:id])
+  end
+  
+end
