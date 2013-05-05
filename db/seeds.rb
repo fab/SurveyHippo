c93b9bf144ac904f45b121a78f39f9d962bb962f
fmackojc <fab.mackojc@gmail.com>
2013-05-05 14:12:25 -0700

    updated seed file and fixed survey results page in line with new table field names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,23 @@
-100.times do
-  User.create(:username => Faker::Name.first_name, :password => 'password', :email => Faker::Internet.email)
+# 100.times do
+#   User.create(:username => Faker::Name.first_name, :password => 'password', :email => Faker::Internet.email)
+# end
+
+# 100.times do
+#   Survey.create(:author_id => User.all.sample.id, :name => Faker::Company.bs)
+# end
+
+# 500.times do
+#   Question.create(:survey_id => Survey.all.sample.id, :text => Faker::Company.catch_phrase)
+# end
+
+# 1000.times do
+#   Choice.create(:question_id => Question.all.sample.id, :text => Faker::Lorem.sentence)
+# end
+
+1.upto(100) do |i|
+  Response.create(:question_id => 501, :choice_id => [1001, 1002].sample, :user_id => i)
 end
 
-100.times do
-  Survey.create(:author_id => User.all.sample.id, :name => Faker::Company.bs)
-end
-
-500.times do
-  Question.create(:survey_id => Survey.all.sample.id, :question => Faker::Company.catch_phrase)
-end
-
-1000.times do
-  Choice.create(:question_id => Question.all.sample.id, :choice => Faker::Lorem.sentence)
-end
-
-100.times do |i|
-  Response.create(:question_id => 519, :choice_id => 1025, :user_id => i)
-end
-
-100.times do |i|
-  Response.create(:question_id => 520, :choice_id => [1026, 1027, 1028].sample, :user_id => i)
-end
-
-100.times do |i|
-  Response.create(:question_id => 521, :choice_id => [1029, 1030].sample, :user_id => i)
+1.upto(100) do |i|
+  Response.create(:question_id => 502, :choice_id => [1003, 1004].sample, :user_id => i)
 end
