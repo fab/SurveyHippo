ce1e68999d570599d6dc941aa0cdf1ce743ad843
fmackojc <fab.mackojc@gmail.com>
2013-05-05 22:20:04 -0700

    login and register buttons now transition nicely on homepage

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,11 @@
 var login_click = function(e) {
   e.preventDefault();
-  $('.register').hide();
-  $('#login_box').show();
+  $('#login_box').slideToggle();
 };
 
 var register_click = function(e) {
   e.preventDefault();
-  $('.login').hide();
-  $('#register_box').show();
+  $('#register_box').slideToggle();
 };
 
 
@@ -17,7 +15,7 @@ $(document).ready(function() {
   $('.login').on('click', function(e){
     login_click(e);
   });
-  
+
   $('.register').on('click', function(e){
     register_click(e);
   });
