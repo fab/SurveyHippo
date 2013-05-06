eeb9ba096399649419504e5ea9482d626c5803fa
fmackojc <fab.mackojc@gmail.com>
2013-05-05 21:52:11 -0700

    login box now appears without needing to route to another page

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,13 @@
 var login_click = function(e) {
   e.preventDefault();
   $('.register').hide();
+  $('#login_box').show();
 };
 
 
 $(document).ready(function() {
+  $('.hidden').hide();
+
   $('.login').on('click', function(e){
     login_click(e);
   });
